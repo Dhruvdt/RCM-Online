86c92bdae9bcb89390f17d8caeeafc0df6fd27c8
dhruvtyagi88 <dhruvtyagi88@gmail.com>
2020-07-05 22:39:38 +0530

    First commit

---
+++ app/src/main/java/com/dhruvdt/rsmonline/SignInOptions.java
@@ -0,0 +1,18 @@
+package com.dhruvdt.rsmonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+
+public class SignInOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_options);
+    }
+}
---
+++ app/src/main/java/com/dhruvdt/rsmonline/SignUpOptions.java
@@ -0,0 +1,14 @@
+package com.dhruvdt.rsmonline;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_options);
+    }
+}
